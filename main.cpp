bc2686c54708bec9ddba43987502a325bc763f1c
Kagiso <64711851+Kgs-Mathaba@users.noreply.github.com>
2021-03-11 21:04:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloWorld
+//
+//  Created by Modikai on 2021/03/08.
+//  Copyright © 2021 Modikai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
